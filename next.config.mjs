e4315b74b4274aa06e0e052660226556f22dc4f2
Soumya Singh <11soumyasingh2@gmail.com>
2025-12-06 20:59:46 +0530

    Routine updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-    domains: ['images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
